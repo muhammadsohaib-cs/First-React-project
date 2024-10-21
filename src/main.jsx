a07bd7211c72e99a9b485b069f33ded8df8ef2bf
muhammad-sohaib <muhammadsohaib.19477@gmail.com>
2024-10-21 23:55:39 +0500

    First project

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import App from './App.jsx'
+//import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <div>
+  <h1>Hello My Name is Muhamad Sohaib</h1>
+  <p>Hello this is my first React project.</p>
+  </div>
+)
+
